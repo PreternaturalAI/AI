6d9143accb8ab639ef5bb914e98fbac9fb2df990
Vatsal Manot <vatsal.manot@yahoo.com>
2024-02-19 23:37:59 -0800

    Update package

--- Sources/CoreMI/Intramodular/Foundation/MIContext.swift
+++ Sources/CoreMI/Intramodular/Foundation/MIContext.swift
@@ -2,12 +2,15 @@
 // Copyright (c) Vatsal Manot
 //
 
+import Combine
 import Swift
 
 /// A context for machine intelligence.
-public final class MIContext {
-    func add<T: _MIRequestHandling>(_ x: T) {
+public final class MIContext: ObservableObject {
+    @Published public var handlers: [any _MIRequestHandling] = []
         
+    public func add<T: _MIRequestHandling>(_ x: T) {
+        handlers.append(x)
     }
 }
 
