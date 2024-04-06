4dec9faebda6387ae1c2b84cd8c731fc8aecd14d
Vatsal Manot <vatsal.manot@yahoo.com>
2024-04-06 10:24:07 -0700

    Update package

--- Package.swift
+++ Package.swift
@@ -147,23 +147,24 @@ let package = Package(
         .testTarget(
             name: "LargeLanguageModelsTests",
             dependencies: [
-                "LargeLanguageModels"
+                "AI",
+                "Swallow"
             ],
             path: "Tests/LargeLanguageModels"
         ),
         .testTarget(
             name: "AnthropicTests",
             dependencies: [
-                "Anthropic",
-                "LargeLanguageModels"
+                "AI",
+                "Swallow"
             ],
             path: "Tests/Anthropic"
         ),
         .testTarget(
             name: "OpenAITests",
             dependencies: [
-                "LargeLanguageModels",
-                "OpenAI"
+                "AI",
+                "Swallow"
             ],
             path: "Tests/OpenAI"
         )
