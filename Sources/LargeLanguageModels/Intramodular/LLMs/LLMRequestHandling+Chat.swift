ef417ad7097f8bc88e188bf07e774fea5120d4f6
Vatsal Manot <vatsal.manot@yahoo.com>
2024-04-21 23:55:04 -0700

    Update package

--- Sources/LargeLanguageModels/Intramodular/LLMs/LLMRequestHandling+Chat.swift
+++ Sources/LargeLanguageModels/Intramodular/LLMs/LLMRequestHandling+Chat.swift
@@ -10,7 +10,45 @@ extension LLMRequestHandling {
         _ messages: [AbstractLLM.ChatMessage],
         parameters: AbstractLLM.ChatCompletionParameters
     ) async throws -> AbstractLLM.ChatCompletion {
-        try await complete(prompt: AbstractLLM.ChatPrompt(messages: messages), parameters: parameters)
+        try await complete(
+            prompt: AbstractLLM.ChatPrompt(messages: messages),
+            parameters: parameters
+        )
+    }
+    
+    public func complete(
+        _ messages: [AbstractLLM.ChatMessage],
+        parameters: AbstractLLM.ChatCompletionParameters,
+        model: some _MLModelIdentifierConvertible
+    ) async throws -> AbstractLLM.ChatCompletion {
+        var prompt = AbstractLLM.ChatPrompt(messages: messages)
+        
+        prompt.context = try withMutableScope(prompt.context) { context in
+            context.completionType = .chat
+            context.modelIdentifier = try .one(model.__conversion())
+        }
+        
+        let completion = try await complete(
+            prompt: prompt,
+            parameters: parameters
+        )
+        
+        return completion
+    }
+    
+    /// Stream a completion for a given chat prompt.
+    public func completion(
+        for prompt: AbstractLLM.ChatPrompt,
+        model: some _MLModelIdentifierConvertible
+    ) async throws -> AbstractLLM.ChatCompletionStream {
+        var prompt = prompt
+        
+        prompt.context = try withMutableScope(prompt.context) { context in
+            context.completionType = .chat
+            context.modelIdentifier = try .one(model.__conversion())
+        }
+        
+        return try await completion(for: prompt)
     }
     
     /// Stream a completion for a given chat prompt.
@@ -28,12 +66,22 @@ extension LLMRequestHandling {
         
         return try await completion(for: prompt)
     }
-    
+}
+
+extension LLMRequestHandling {
     /// Stream a completion for a given chat prompt.
     public func stream(
         _ prompt: AbstractLLM.ChatPrompt
     ) async throws -> AbstractLLM.ChatCompletionStream {
         try await completion(for: prompt)
+    }
+    
+    /// Stream a completion for a given chat prompt.
+    public func stream(
+        _ prompt: AbstractLLM.ChatPrompt,
+        model: some _MLModelIdentifierConvertible
+    ) async throws -> AbstractLLM.ChatCompletionStream {
+        try await completion(for: prompt, model: model)
     }
     
     /// Stream a completion for a given chat prompt and a desired model.
