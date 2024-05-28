1d084a8ce28a9f8f5e2a90ba785abc0dc65965ab
Natasha <nmurashev@gmail.com>
2024-05-28 17:58:51 +0530

    fixed anthropic tests

--- Tests/Anthropic/module.swift
+++ Tests/Anthropic/module.swift
@@ -10,6 +10,6 @@ public var ANTHROPIC_API_KEY: String {
     ""
 }
 
-public var client: Anthropic {
-    Anthropic(apiKey: ANTHROPIC_API_KEY)
+public var client: Anthropic.Client {
+    Anthropic.Client(apiKey: ANTHROPIC_API_KEY)
 }
