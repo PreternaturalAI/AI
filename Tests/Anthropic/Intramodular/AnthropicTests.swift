dbec21d44eb3d61e21723bac12fdbfd6db8335fc
NatashaTheRobot <nmurashev@gmail.com>
2024-06-23 15:34:14 +0530

    added image support for anthropic

--- Tests/Anthropic/Intramodular/AnthropicTests.swift
+++ Tests/Anthropic/Intramodular/AnthropicTests.swift
@@ -5,6 +5,7 @@
 import Anthropic
 import LargeLanguageModels
 import XCTest
+import SwiftUIX
 
 final class AnthropicTests: XCTestCase {    
     func test() async throws {
@@ -27,6 +28,42 @@ final class AnthropicTests: XCTestCase {
         )
         
         print(completion)
+    }
+    
+    func testVision() async throws {
+       let imageData = try await downloadAndEncodeImage(from: "https://upload.wikimedia.org/wikipedia/en/7/77/EricCartman.png")
+        let image = AppKitOrUIKitImage(data: imageData)!
+        
+        let model = Anthropic.Model.sonnet
+        
+        let systemPrompt: PromptLiteral = "You are a South Park Character experct. Your job is to analyze the given image of a South Park Character and identify which character it is. Make sure to provide the full name of the character."
+
+        let userPrompt: PromptLiteral = "Which character is this?"
+
+        do {
+            let imageLiteral = try PromptLiteral(image: image)
+            
+            let messages: [AbstractLLM.ChatMessage] = [
+                .system(systemPrompt),
+                .user {
+                    .concatenate(separator: nil) {
+                        userPrompt
+                        imageLiteral
+                    }
+                }]
+            
+            let result: String = try await client.complete(
+                messages,
+                model: model,
+                as: .string
+            )
+            
+            print(result)
+            XCTAssertTrue(result.contains("Eric Cartman"))
+        } catch {
+            print(error)
+            XCTFail(error.localizedDescription)
+        }
     }
 }
 
@@ -61,4 +98,19 @@ func generateSwiftUICode(requirement: PromptLiteral) -> [AbstractLLM.ChatMessage
         )
     ]
 }
+
+fileprivate func downloadAndEncodeImage(
+    from urlString: String
+) async throws -> Data {
+    guard let url = URL(string: urlString) else {
+        throw URLError(.badURL)
+    }
+    
+    // Using async/await to download data
+    let (data, _) = try await URLSession.shared.data(from: url)
+    
+    return data
+}
+
+
 
