cf8952a265c58acca9d8f11502c3a072bcd28c69
Vatsal Manot <vatsal.manot@yahoo.com>
2024-11-23 04:07:20 +0530

    Update package

--- Sources/CoreMI/Intramodular/TTS/SpeechSynthesizers.swift
+++ Sources/CoreMI/Intramodular/TTS/SpeechSynthesizers.swift
@@ -8,18 +8,3 @@ import Swift
 public enum SpeechSynthesizers {
     
 }
-
-extension SpeechSynthesizers {
-    public protocol VoiceName: Codable, Hashable, Identifiable {
-        
-    }
-}
-
-extension SpeechSynthesizers {
-    public struct AnyVoiceName: SpeechSynthesizers.VoiceName {
-        public var id: AnyPersistentIdentifier
-        public var displayName: String
-        public var userInfo: UserInfo
-    }
-}
-
