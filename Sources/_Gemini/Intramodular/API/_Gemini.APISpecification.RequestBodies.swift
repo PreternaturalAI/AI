05c030f6db7bf95fc0d63e0b045b10816f91d75c
Jared Davidson (Archetapp) <archetapp@gmail.com>
2024-12-11 14:34:32 -0700

    initial

---
+++ Sources/_Gemini/Intramodular/API/_Gemini.APISpecification.RequestBodies.swift
@@ -0,0 +1,7 @@
+//
+//  _Gemini.APISpecification.RequestBodies.swift
+//  AI
+//
+//  Created by Jared Davidson on 12/11/24.
+//
+
---
+++ Sources/_Gemini/Intramodular/Models/_Gemini.Model.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  AI
+//
+//  Created by Jared Davidson on 12/11/24.
+//
+
