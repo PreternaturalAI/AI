3d684484a0fbec77cbbc77447ad66eaa7d70da66
Vatsal Manot <vatsal.manot@yahoo.com>
2024-12-12 01:00:16 +0530

    Update package

--- Sources/_Gemini/Intramodular/_Gemini.Model.swift
+++ Sources/_Gemini/Intramodular/_Gemini.Model.swift
@@ -7,14 +7,17 @@ import Swift
 
 extension _Gemini {
     public enum Model: String, CaseIterable, Codable, Hashable, Sendable {
+        case gemini_2_0_flash_exp = "gemini-2.0-flash-exp"
         case gemini_1_5_pro = "gemini-1.5-pro"
         case gemini_1_5_pro_latest = "gemini-1.5-pro-latest"
         case gemini_1_5_flash = "gemini-1.5-flash"
         case gemini_1_5_flash_latest = "gemini-1.5-flash-latest"
         case gemini_1_0_pro = "gemini-1.0-pro"
-        
+                
         public var maximumContextLength: Int {
             switch self {
+                case .gemini_2_0_flash_exp:
+                    return 1048576
                 case .gemini_1_5_pro:
                     return 1048576
                 case .gemini_1_5_pro_latest:
